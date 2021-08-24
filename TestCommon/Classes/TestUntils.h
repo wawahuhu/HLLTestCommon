66ed558e8d529b4b8ca32f53d6e18875c726eff7
hulilei <hulilei@jiagouyun.com>
2021-08-24 11:31:14 +0800

    add

---
+++ TestCommon/Classes/TestUntils.h
@@ -0,0 +1,16 @@
+//
+//  TestUntils.h
+//  TestCommon
+//
+//  Created by 胡蕾蕾 on 2021/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestUntils : NSObject
++ (void)testLog;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestCommon/Classes/TestUntils.m
@@ -0,0 +1,14 @@
+//
+//  TestUntils.m
+//  TestCommon
+//
+//  Created by 胡蕾蕾 on 2021/8/24.
+//
+
+#import "TestUntils.h"
+
+@implementation TestUntils
++ (void)testLog{
+  
+}
+@end
